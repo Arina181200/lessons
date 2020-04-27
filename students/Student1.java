646ebd116f86967d0c7ac5c9aaeecda26c8979b9
Arina Davydova <arina.davidova1812@mail.ru>
2020-04-27 06:56:14 +0600

    lesson9

--- students/Student1.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Student1 extends Student {
-    public Student1(String name, int course, int GPA) {
-        super(name, course, GPA);
-    }
-}
--- students/Student3.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public class Student3 extends Student {
-    public Student3(String name, int course, int GPA) {
-        super(name, course, GPA);
-    }
-}
